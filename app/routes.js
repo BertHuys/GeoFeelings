7256d938622f6ba61fdc3e88d78110d5a25e0e4b
BertHuys <bert.huys@student.howest.be>
2016-01-08 17:20:32 +0100

    edit 3 08/01

--- app/routes.js
+++ app/routes.js
@@ -1,43 +1,22 @@
-/**
- * Created by BERT on 3/01/16.
- */
-//Dependencies
+
 var mongoose        = require('mongoose');
-var User            = require('./model.js');
+var Checkin            = require('./model.js');
 
-// Opens App Routes
 module.exports = function(app) {
-
-    // GET Routes
-    // --------------------------------------------------------
-    // Retrieve records for all users in the db
-    app.get('/users', function(req, res){
-
-        // Uses Mongoose schema to run the search (empty conditions)
+    app.get('/checkins', function(req, res){
         var query = User.find({});
         query.exec(function(err, users){
             if(err)
                 res.send(err);
-
-            // If no errors are found, it responds with a JSON of all users
             res.json(users);
         });
     });
 
-    // POST Routes
-    // --------------------------------------------------------
-    // Provides method for saving new users in the db
-    app.post('/users', function(req, res){
-
-        // Creates a new User based on the Mongoose schema and the post bo.dy
-        var newuser = new User(req.body);
-
-        // New User is saved in the db.
-        newuser.save(function(err){
+    app.post('/checkins', function(req, res){
+        var newcheckin = new Checkin(req.body);
+        newcheckin.save(function(err){
             if(err)
                 res.send(err);
-
-            // If no errors are found, it responds with a JSON of the new user
             res.json(req.body);
         });
     });
